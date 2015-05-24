788aecabd08a8638a5f950e7ba5f350b3ab5a88c
Riccardo B. Desantis <effetti@gmail.com>
2015-05-24 16:56:51 +0200

    Added two projects

---
+++ modaclouds-sdatests/src/it/polimi/modaclouds/sdatests/Main.java
@@ -0,0 +1,159 @@
+package it.polimi.modaclouds.sdatests;
+
+import it.cloud.amazon.ec2.Configuration;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+
+public class Main {
+	
+	private static final Logger logger = LoggerFactory.getLogger(Main.class);
+
+	@Parameter(names = { "-h", "--help" }, help = true)
+	private boolean help = false;
+	
+	@Parameter(names = "-size", description = "The size that will be used for all the machines")
+	private String size = null;
+	
+	@Parameter(names = "-clients", description = "The number of JMeter clients to be used (default: 1)")
+	private int clients = 1;
+	
+	@Parameter(names = "-data", description = "The file with the workload")
+	private String data = null;
+	
+	@Parameter(names = "-loadBalancer", description = "The DNS of the load balancer")
+	private String loadBalancer = null;
+	
+	@Parameter(names = "-batch", description = "Go in batch mode, reading a file describing a number of tests as parameter")
+	private String batch = null;
+	
+	@Parameter(names = "-baseJmx", description = "The JMX that will be configured for the run")
+	private String baseJmx = "jmeterTestTemplate.jmx";
+	
+	@Parameter(names = "-useDatabase", description = "Either if we need to use an external database or not")
+	private boolean useDatabase = false;
+	
+	@Parameter(names = "-useOnDemand", description = "Use OnDemand instances instead of OnSpot")
+	private boolean useOnDemand = false;
+	
+	@Parameter(names = "-reuseInstances", description = "Reuse running instances")
+	private boolean reuseInstances = false;
+	
+	@Parameter(names = "-leaveInstancesOn", description = "Leave the instances running")
+	private boolean leaveInstancesOn = false;
+	
+	@Parameter(names = "-background", description = "Run in background", hidden = true)
+	private boolean background = false;
+	
+	@Parameter(names = "-onlyStartMachines", description = "Starts the machines and quit", hidden = true)
+	private boolean onlyStartMachines = false;
+	
+	@Parameter(names = "-wait", description = "Stalls the execution of the given milliseconds without starting any test at all (used only in batch)", hidden = true)
+	private int wait = -1;
+	
+	public static final String APP_TITLE = "\nSDA Test\n";
+
+	public static void main(String[] args) {
+//		args = new String[] { "-clients", "1", "-size", "m3.large", "-data", "tests.txt", "-useOnDemand", "-reuseInstances", "-leaveInstancesOn" };
+		
+		Main m = new Main();
+		JCommander jc = new JCommander(m, args);
+		
+		System.out.println(APP_TITLE);
+		
+		if (m.help) {
+			jc.usage();
+			System.exit(0);
+		}
+		
+		if (m.batch == null && m.data == null) {
+			logger.error("You need to provide a data or batch file!");
+			System.exit(-1);
+		} else if (m.batch == null) {
+			doTest(m.size, m.clients, Paths.get(m.baseJmx), m.data, m.loadBalancer, m.useDatabase, m.useOnDemand, m.reuseInstances, m.leaveInstancesOn, m.onlyStartMachines);
+		} else {
+			ArrayList<Thread> threads = new ArrayList<Thread>(); 
+			
+			try (Scanner sc = new Scanner(Configuration.getInputStream(m.batch))) {
+				while (sc.hasNextLine()) {
+					String line = sc.nextLine();
+					if (line.startsWith("#") || line.trim().length() == 0)
+						continue;
+					
+					String[] fields = line.split(" ");
+					jc = new JCommander(m, fields);
+					
+					if (m.help)
+						continue;
+					
+					if (m.wait > 0) {
+						logger.info("Waiting {} ms...", m.wait);
+						try {
+							Thread.sleep(m.wait);
+						} catch (Exception e) { }
+						m.wait = -1;
+						continue;
+					}
+					
+					if (m.background)
+						threads.add(doTestInBackground(m.size, m.clients, Paths.get(m.baseJmx), m.data, m.loadBalancer, m.useDatabase, m.useOnDemand, m.reuseInstances, m.leaveInstancesOn, m.onlyStartMachines));
+					else
+						doTest(m.size, m.clients, Paths.get(m.baseJmx), m.data, m.loadBalancer, m.useDatabase, m.useOnDemand, m.reuseInstances, m.leaveInstancesOn, m.onlyStartMachines);
+				}
+				
+				for (Thread t : threads)
+					t.join();
+				
+				logger.error("Jobs ended!");
+			} catch (Exception e) {
+				logger.error("Error while reading the file!", e);
+				System.exit(-1);
+			}
+		}
+		
+		System.exit(0);
+		
+	}
+	
+	public static void doTest(String size, int clients, Path baseJmx, String data, String loadBalancer, boolean useDatabase, boolean startAsOnDemand, boolean reuseInstances, boolean leaveInstancesOn, boolean onlyStartMachines) {
+		logger.info("Preparing the system and running the test...");
+		
+		boolean res = Test.performTest(size, clients, baseJmx, data, loadBalancer, useDatabase, startAsOnDemand, reuseInstances, leaveInstancesOn, onlyStartMachines);
+		
+		if (res)
+			logger.info("The test run correctly!");
+		else
+			logger.error("There were some problems during the test! :(");
+	}
+	
+	public static Thread doTestInBackground(String size, int clients, Path baseJmx, String data, String loadBalancer, boolean useDatabase, boolean startAsOnDemand, boolean reuseInstances, boolean leaveInstancesOn, boolean onlyStartMachines) {
+		final String fsize = size;
+		final int fclients = clients;
+		final Path fbaseJmx = baseJmx;
+		final String fdata = data;
+		final String floadBalancer = loadBalancer;
+		final boolean fuseDatabase = useDatabase;
+		final boolean fstartAsOnDemand = startAsOnDemand;
+		final boolean freuseInstances = reuseInstances;
+		final boolean fleaveInstancesOn = leaveInstancesOn;
+		final boolean fonlyStartMachines = onlyStartMachines;
+		
+		Thread t = new Thread() {
+			public void run() {
+				doTest(fsize, fclients, fbaseJmx, fdata, floadBalancer, fuseDatabase, fstartAsOnDemand, freuseInstances, fleaveInstancesOn, fonlyStartMachines);
+			}
+		};
+		
+		t.start();
+		return t;
+	}
+
+}
---
+++ modaclouds-sdatests/src/it/polimi/modaclouds/sdatests/Test.java
@@ -0,0 +1,331 @@
+package it.polimi.modaclouds.sdatests;
+
+import it.cloud.amazon.ec2.AmazonEC2;
+import it.cloud.amazon.ec2.Configuration;
+import it.cloud.amazon.ec2.VirtualMachine;
+import it.cloud.amazon.ec2.VirtualMachine.Instance;
+import it.cloud.utils.CloudException;
+import it.cloud.utils.JMeterTest;
+import it.cloud.utils.JMeterTest.RunInstance;
+import it.cloud.utils.Ssh;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Scanner;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Test {
+	
+	private static final Logger logger = LoggerFactory.getLogger(Test.class);
+	
+	private VirtualMachine mpl;
+	private VirtualMachine sda;
+	private VirtualMachine mic;
+	private VirtualMachine clients;
+	private VirtualMachine database;
+	
+	boolean useDatabase;
+	
+	private boolean running;
+	private boolean initialized;
+	
+	static {
+		VirtualMachine.PRICE_MARGIN = 0.35;
+	}
+	
+	public static boolean performTest(String size, int clients, Path baseJmx, String data, String loadBalancer, boolean useDatabase, boolean startAsOnDemand, boolean reuseInstances, boolean leaveInstancesOn, boolean onlyStartMachines) {
+		try {
+			Test t = new Test(size, clients, useDatabase);
+			if (reuseInstances)
+				t.considerRunningMachines();
+			t.startMachines(startAsOnDemand);
+			if (onlyStartMachines)
+				return true;
+			t.initSystem();
+			t.runTest(baseJmx, data, loadBalancer);
+			if (!leaveInstancesOn)
+				t.stopMachines();
+			return true;
+		} catch (Exception e) {
+			logger.error("There were errors while running the test.", e);
+			return false;
+		}
+	}
+
+	public Test(String size, int clients, boolean useDatabase) throws CloudException {
+		if (clients <= 0)
+			throw new RuntimeException("You need at least 1 client!");
+		
+		mpl = VirtualMachine.getVM("mpl", size, 1);
+		sda = VirtualMachine.getVM("sda", size, 1);
+		mic = VirtualMachine.getVM("mic", size, 1);
+		this.clients = VirtualMachine.getVM("client", size, clients);
+		
+		this.useDatabase = useDatabase;
+		if (useDatabase)
+			database = VirtualMachine.getVM("database", size, 1);
+		
+		running = false;
+		initialized = false;
+		
+		otherThreads = new ArrayList<Thread>();
+	}
+	
+	public void startMachines(boolean startAsOnDemand) {
+		if (running)
+			throw new RuntimeException("The system is already running!");
+		
+		logger.info("Starting all the machines...");
+		
+		if (!startAsOnDemand) {
+			mpl.spotRequest();
+			sda.spotRequest();
+			mic.spotRequest();
+			clients.spotRequest();
+			if (useDatabase)
+				database.spotRequest();
+		} else {
+			mpl.onDemandRequest();
+			sda.onDemandRequest();
+			mic.onDemandRequest();
+			clients.onDemandRequest();
+			if (useDatabase)
+				database.onDemandRequest();
+		}
+		
+		mpl.waitUntilRunning();
+		sda.waitUntilRunning();
+		mic.waitUntilRunning();
+		clients.waitUntilRunning();
+		if (useDatabase)
+			database.waitUntilRunning();
+		
+		mpl.setNameToInstances("MPL");
+		sda.setNameToInstances("SDA");
+		mic.setNameToInstances("MiC");
+		clients.setNameToInstances("JMeter");
+		
+		mpl.getInstances().get(0).waitUntilSshAvailable();
+		sda.getInstances().get(0).waitUntilSshAvailable();
+		mic.getInstances().get(0).waitUntilSshAvailable();
+		if (useDatabase)
+			database.getInstances().get(0).waitUntilSshAvailable();
+		
+		logger.info("All the machines are now running!");
+		
+		running = true;
+	}
+	
+	public void considerRunningMachines() {
+		if (running)
+			throw new RuntimeException("The system is already running!");
+		
+		logger.info("Searching for running machines...");
+		
+		AmazonEC2 ec2 = new AmazonEC2();
+		
+		ec2.addRunningInstances(mpl);
+		ec2.addRunningInstances(sda);
+		ec2.addRunningInstances(mic);
+		ec2.addRunningInstances(clients);
+		if (useDatabase)
+			ec2.addRunningInstances(database);
+		
+		mpl.reboot();
+		sda.reboot();
+		mic.reboot();
+		clients.reboot();
+		if (useDatabase)
+			database.reboot();
+		
+		logger.info("Added running instances!");
+	}
+	
+	public void stopMachines() {
+		if (!running)
+			throw new RuntimeException("The system isn't running yet!");
+		
+		logger.info("Stopping all the machines...");
+		
+		mpl.terminate();
+		sda.terminate();
+		mic.terminate();
+		clients.terminate();
+		if (useDatabase)
+			database.terminate();
+		
+		logger.info("All the machines have been shutted down!");
+		
+		running = false;
+		initialized = false;
+	}
+	
+	public static final String LOAD_MODEL_FILE = "MPloadModel";
+	public static final String OBSERVER_LAUNCH_FILE= "observer_remote_launcher";
+	
+	public static String LOAD_MODEL_COMMAND = "bash " + Configuration.getPathToFile(LOAD_MODEL_FILE) + " %s %s";
+	public static String OBSERVER_LAUNCH_COMMAND = "bash " + Configuration.getPathToFile(OBSERVER_LAUNCH_FILE) + " %s %s %s";
+	
+	private List<Thread> otherThreads;
+	
+	public void initSystem() throws Exception {
+		if (!running)
+			throw new RuntimeException("The system isn't running yet!");
+		
+		if (initialized)
+			throw new RuntimeException("The system is already initialized!");
+		
+		logger.info("Deleting the files set for removal before the test...");
+		
+		sda.deleteFiles();
+		mic.deleteFiles();
+		mpl.deleteFiles();
+		clients.deleteFiles();
+		if (useDatabase)
+			database.deleteFiles();
+		
+		logger.info("Initializing the system...");
+		
+		Instance impl = mpl.getInstances().get(0);
+		Instance isda = sda.getInstances().get(0);
+		Instance imic = mic.getInstances().get(0);
+		
+		impl.exec(mpl.getParameter("STARTER"));
+		
+		try { Thread.sleep(10000); } catch (Exception e) { }
+		
+		exec(String.format(
+				LOAD_MODEL_COMMAND,
+				impl.getIp(),
+				isda.getIp()));
+		
+		try { Thread.sleep(10000); } catch (Exception e) { }
+		
+		exec(String.format(
+				OBSERVER_LAUNCH_COMMAND,
+				isda.getIp(),
+				Configuration.getPathToFile(sda.getParameter("KEYPAIR_NAME") + ".pem").toString(),
+				sda.getParameter("SSH_USER")));
+		
+		try { Thread.sleep(10000); } catch (Exception e) { }
+		
+		otherThreads.add(Ssh.execInBackground(isda, String.format(
+				sda.getParameter("STARTER"),
+				impl.getIp())));
+		
+		try { Thread.sleep(10000); } catch (Exception e) { }
+		
+		imic.exec(String.format(
+				mic.getParameter("STARTER0"),
+				useDatabase ? database.getIps().get(0) : "127.0.0.1",
+				impl.getIp()
+				));
+		
+		try { Thread.sleep(10000); } catch (Exception e) { }
+		
+		imic.exec(mic.getParameter("STARTER1"));
+		
+		try { Thread.sleep(10000); } catch (Exception e) { }
+		
+		logger.info("System initialized!");
+		
+		initialized = true;
+	}
+	
+	public static List<String> exec(String command) throws IOException {
+		List<String> res = new ArrayList<String>();
+		ProcessBuilder pb = new ProcessBuilder(command.split(" "));
+		pb.redirectErrorStream(true);
+		
+		Process p = pb.start();
+		BufferedReader stream = new BufferedReader(new InputStreamReader(p.getInputStream()));
+		String line = stream.readLine(); 
+		while (line != null) {
+			logger.trace(line);
+			res.add(line);
+			line = stream.readLine();
+		}
+		stream.close();
+	
+		return res;
+	}
+	
+	public static int getPeakFromData(String data) {
+		int peak = 0;
+		try (Scanner sc = new Scanner(Configuration.getInputStream(data))) {
+			while (sc.hasNextLine()) {
+				String line = sc.nextLine();
+				int value;
+				try {
+					value = Integer.parseInt(line.split(" ")[0]);
+				} catch (Exception e) {
+					continue;
+				}
+				if (value > peak)
+					peak = value;
+			}
+		} catch (Exception e) { }
+		return peak;
+	}
+	
+	public void runTest(Path baseJmx, String data, String loadBalancerDNS) throws Exception {
+		if (!running)
+			throw new RuntimeException("The system isn't running yet!");
+		
+		if (!initialized)
+			throw new RuntimeException("The system isn't initialized yet!");
+		
+		Date date = new Date();
+		String now = String.format("%1$td%1$tm%1$ty%1$tH%1$tM-%2$s-%3$dx%4$d", date, clients.getSize(), getPeakFromData(data) / clients.getInstancesRunning(), clients.getInstancesRunning());
+		
+		String localPath = "tests" + File.separator + now;
+		if (!new File(localPath).exists()) {
+			File f = new File(localPath);
+			f.mkdirs();
+		}
+		
+		String remotePath = clients.getParameter("REMOTE_PATH") + "/" + now;
+		
+		String protocol = mic.getParameter("PROTOCOL");
+		if (protocol == null)
+			protocol = "http";
+		
+		String port = mic.getParameter("PORT");
+		if (port == null)
+			port = "8080";
+		
+		
+		JMeterTest test = new JMeterTest(clients.getParameter("AMI"), clients.getInstancesRunning(), localPath, remotePath, clients.getParameter("JMETER_PATH"), data,
+				loadBalancerDNS != null ? loadBalancerDNS : mic.getIps().get(0),
+				protocol,
+				port);
+		
+		RunInstance run = test.createModifiedFile(baseJmx);
+		
+		logger.info("Test starting...");
+		
+		test.performTest(clients, run);
+		
+		logger.info("Test ended!");
+		
+		logger.info("Retrieving the files from the instances...");
+		
+		sda.retrieveFiles(localPath, "/home/" + sda.getParameter("SSH_USER"));
+		mic.retrieveFiles(localPath, "/home/" + mic.getParameter("SSH_USER"));
+		mpl.retrieveFiles(localPath, "/home/" + mpl.getParameter("SSH_USER"));
+		clients.retrieveFiles(localPath, "/home/" + clients.getParameter("SSH_USER"));
+		if (useDatabase)
+			database.retrieveFiles(localPath, "/home/" + database.getParameter("SSH_USER"));
+		
+		logger.info("Done!");
+	}
+
+}
