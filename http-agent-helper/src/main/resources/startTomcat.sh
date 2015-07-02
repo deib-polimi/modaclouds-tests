76cf91ef51d1eafb35f1115bd6eac970a18d875b
Riccardo B. Desantis <effetti@gmail.com>
2015-07-02 14:26:20 +0200

    Bugfix

--- http-agent-helper/src/main/resources/startTomcat.sh
+++ http-agent-helper/src/main/resources/startTomcat.sh
@@ -6,6 +6,8 @@ IP="$1"
 cd /home/ubuntu/modaclouds
 sudo ./httpAgentRun.sh
 
+sleep 10
+
 ssh root@localhost -o StrictHostKeyChecking=no -p $PORT -i /home/ubuntu/modaclouds/id_rsa "bash /usr/local/bin/actualStartTomcat.sh $IP"
 
 cd
--- http-agent-helper/src/main/resources/stopTomcat.sh
+++ http-agent-helper/src/main/resources/stopTomcat.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 cd /home/ubuntu/modaclouds
-sudo docker kill httpagent16
+sudo docker kill `sudo docker ps | grep httpagent16 | awk '{print $1}'`
 
 cd
