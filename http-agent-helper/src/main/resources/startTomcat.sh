acc1bdb86d01fb3edd06309d1f189a4894edc664
Riccardo B. Desantis <effetti@gmail.com>
2015-07-02 16:03:52 +0200

    Bugfix

--- http-agent-helper/src/main/resources/startTomcat.sh
+++ http-agent-helper/src/main/resources/startTomcat.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+#$1=MP-IP
+#$2=OBSERVER-IP
+#$3=OBSERVER-PORT
 
 PORT="8022"
 IP="$1"
@@ -9,5 +12,8 @@ sudo ./httpAgentRun.sh
 sleep 10
 
 ssh root@localhost -o StrictHostKeyChecking=no -p $PORT -i /home/ubuntu/modaclouds/id_rsa "bash /usr/local/bin/actualStartTomcat.sh $IP"
+
+cd /home/ubuntu/http-agent-helper
+bash MPloadModel $1 $2 $3
 
 cd
