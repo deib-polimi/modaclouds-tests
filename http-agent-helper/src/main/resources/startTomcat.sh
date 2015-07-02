bf6529f065d0bd815430ec456dfaa4af5a9199b5
Riccardo B. Desantis <effetti@gmail.com>
2015-07-02 14:07:29 +0200

    Uff

--- http-agent-helper/src/main/resources/startTomcat.sh
+++ http-agent-helper/src/main/resources/startTomcat.sh
@@ -3,6 +3,7 @@
 PORT="8022"
 IP="$1"
 
+cd /home/ubuntu/modaclouds
 sudo ./httpAgentRun.sh
 
 ssh root@localhost -o StrictHostKeyChecking=no -p $PORT -i /home/ubuntu/modaclouds/id_rsa "bash /usr/local/bin/actualStartTomcat.sh $IP"
