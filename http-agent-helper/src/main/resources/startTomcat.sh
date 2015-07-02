d611c653aa47f887bb819d36bc4bb222fdd6b448
Riccardo B. Desantis <effetti@gmail.com>
2015-07-02 13:38:04 +0200

    Bugfixes

--- http-agent-helper/src/main/resources/startTomcat.sh
+++ http-agent-helper/src/main/resources/startTomcat.sh
@@ -3,6 +3,9 @@
 PORT="8022"
 IP="$1"
 
-bash /home/ubuntu/modaclouds/build.sh httpagent 16
+cd /home/ubuntu/modaclouds
+bash build.sh httpagent 16
 
 ssh root@localhost -o StrictHostKeyChecking=no -p $PORT -i /home/ubuntu/modaclouds/id_rsa "bash /usr/local/bin/actualStartTomcat.sh $IP"
+
+cd
