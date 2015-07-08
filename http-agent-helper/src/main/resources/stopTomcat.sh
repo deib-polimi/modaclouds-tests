a45de86d4c3fcf45807966fcf4923f9094110e1c
Riccardo B. Desantis <effetti@gmail.com>
2015-07-08 19:33:00 +0200

    Bugfix

--- http-agent-helper/src/main/resources/stopTomcat.sh
+++ http-agent-helper/src/main/resources/stopTomcat.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+cd
+
+ID=`sudo docker ps | grep httpagent16 | awk '{print $1}'`
+
+rm -rf logs
+mkdir -p logs
+docker cp $ID:/var/lib/tomcat7/logs/* /home/ubuntu/logs/
+
 cd /home/ubuntu/modaclouds
 
 sudo docker kill `sudo docker ps | grep httpagent16 | awk '{print $1}'`
