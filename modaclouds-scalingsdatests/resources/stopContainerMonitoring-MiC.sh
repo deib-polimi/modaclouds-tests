5aa17b1963f8f3cb307db3d237e7143706e0cdea
Riccardo B. Desantis <effetti@gmail.com>
2015-07-15 16:49:09 +0200

    Now it creates the folder before trying to write into it.

--- modaclouds-scalingsdatests/resources/stopContainerMonitoring-MiC.sh
+++ modaclouds-scalingsdatests/resources/stopContainerMonitoring-MiC.sh
@@ -2,6 +2,8 @@
 #$1=IP MIC
 #$2=FOLDER
 
+mkdir -p "$2"
+
 curl -X GET -k -H "Accept: application/json" -H "X-Requested-By: GlassFish REST HTML interface" -H "Authorization: Basic YWRtaW46ZGVpYi1wb2xpbWk=" https://"$1":4848/monitoring/domain/server/applications/mic-frontend-mon/server/RegisterServlet > "$2"/reg.json
 curl -X GET -k -H "Accept: application/json" -H "X-Requested-By: GlassFish REST HTML interface" -H "Authorization: Basic YWRtaW46ZGVpYi1wb2xpbWk=" https://"$1":4848/monitoring/domain/server/applications/mic-frontend-mon/server/AnswerQuestionsServlet > "$2"/answ.json
 curl -X GET -k -H "Accept: application/json" -H "X-Requested-By: GlassFish REST HTML interface" -H "Authorization: Basic YWRtaW46ZGVpYi1wb2xpbWk=" https://"$1":4848/monitoring/domain/server/applications/mic-frontend-mon/server/SaveAnswerServlet > "$2"/save.json
