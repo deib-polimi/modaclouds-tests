9e4feccfae8869c4b35ee64cc20fc30348dffea8
Riccardo B. Desantis <effetti@gmail.com>
2015-07-16 11:12:40 +0200

    Removed the sdatests because fully integrated in scalingsdatests

--- modaclouds-sdatests/resources/startContainerMonitoring-MiC.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-#$1=IP MIC
-
-curl -X POST -k -H "Accept: application/json" -H "X-Requested-By: GlassFish REST HTML interface" -H "Authorization: Basic YWRtaW46ZGVpYi1wb2xpbWk=" https://"$1":4848/management/domain/enable-monitoring -d "target=server" -d "modules=web-container"
--- modaclouds-sdatests/resources/stopContainerMonitoring-HTTPAgent.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#$1=IP HTTPAgent
-#$2=FOLDER
-#$3=KEY
-
-ssh -i $3 -o StrictHostKeyChecking=no ubuntu@"$1" 'bash /home/ubuntu/http-agent-helper/stopTomcat.sh'
