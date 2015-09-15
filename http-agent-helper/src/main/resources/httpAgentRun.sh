76215dd84dd91a7783415f8c31670e944ba3ac91
Riccardo B. Desantis <effetti@gmail.com>
2015-09-15 18:28:50 +0200

    aaa

--- http-agent-helper/src/main/resources/httpAgentRun.sh
+++ http-agent-helper/src/main/resources/httpAgentRun.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 
-sudo docker run -d -p 80:80 -p 443:443 -p 8022:22 -p 8080:8080 httpagent16
-
-echo Use port 80 to connect to web server and 8022 to ssh into container
+sudo docker run -d -p 8080:8080 httpagent16
